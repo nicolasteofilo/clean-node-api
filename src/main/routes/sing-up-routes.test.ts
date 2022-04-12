ad6203707511c289b1cef789d77c0ce953c4620a
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-12 14:46:16 -0300

    test: ensure mongodb reconnect if clien is down

--- src/main/routes/sing-up-routes.test.ts
+++ src/main/routes/sing-up-routes.test.ts
@@ -13,7 +13,7 @@ describe('SingUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
