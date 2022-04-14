c89c6268f11b4e16864b3c912e88cfbe11000e53
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-14 08:42:29 -0300

    refactor: move sut creation LogMongoRepository  to a factory method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -23,7 +23,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an log error on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
