5a03edbedaded02005aecbae92871c4070229eb9
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-19 16:50:02 -0300

    chore: update eslint and prettier configs

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,7 @@
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provider', () => {
+    const sut = new LoginController()
+  })
+})
