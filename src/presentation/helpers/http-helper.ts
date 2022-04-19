626e20017841f34ecfb3b238344e6338ac7cbbfc
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-19 18:57:00 -0300

    feat: ensure LoginController returns 401 if invalid credentials

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,23 +1,22 @@
 import { HttpResponse } from '../protocols/'
-import { ServerError } from '../errors/'
+import { ServerError, UnauthorizedError } from '../errors/'
 
-export const badRequest = (error: Error): HttpResponse => (
-  {
-    statusCode: 400,
-    body: error
-  }
-)
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+})
 
-export const serverError = (error: Error): HttpResponse => (
-  {
-    statusCode: 500,
-    body: new ServerError(error.stack)
-  }
-)
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack),
+})
 
-export const ok = (data: any, statusCode = 200): HttpResponse => (
-  {
-    statusCode: statusCode,
-    body: data
-  }
-)
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError(),
+})
+
+export const ok = (data: any, statusCode = 200): HttpResponse => ({
+  statusCode: statusCode,
+  body: data,
+})
