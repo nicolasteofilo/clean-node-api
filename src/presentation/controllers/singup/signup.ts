b8e5e3f1ebf94ca4bb7caf2fd5205498b0cf5f04
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-21 12:58:40 -0300

    refactor: remove an used tests

--- src/presentation/controllers/singup/signup.ts
+++ src/presentation/controllers/singup/signup.ts
@@ -1,7 +1,7 @@
 import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount, Validation } from './singup-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
-import { MissingParamError, InvalidParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -19,13 +19,6 @@ export class SignUpController implements Controller {
       const error = this.validation.validate(httpRequest.body)
       if (error) {
         return badRequest(error)
-      }
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
       }
 
       const { name, email, password, passwordConfirmation } = httpRequest.body
