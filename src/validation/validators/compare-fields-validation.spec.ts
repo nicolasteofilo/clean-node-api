52bcaba262d37b50f8c122cdde3994764a4d6678
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-05 20:27:17 -0300

    refactor: move validation to its ow layer

--- src/validation/validators/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields-validation.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../presentation/errors'
 import { CompareFieldValidation } from './compare-fields-validation'
 
 const makeSut = (): CompareFieldValidation => {
