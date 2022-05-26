ac5d3b8c64cee04849c94fcc17e5264f7b944dd8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-25 21:10:30 -0300

    test: ensure DbLoadSurveys throw if LoadSurveysRepository throws

--- src/data/useCases/load-surveys/db-load-surveys.spec.ts
+++ src/data/useCases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,7 @@
 import { SurveyModel } from '../../../domain/models/survey'
 import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
 import { DbLoadSurveys } from './db-load-surveys'
+import MockDate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => ([{
     question: 'any_question',
@@ -37,10 +38,33 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbLoadSurveys', () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+
     test('Should call LoadSurveysRepository', async () => {
         const { sut, loadSurveysRepositoryStub } = makeSut()
         const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
         await sut.load()
         expect(loadAllSpy).toHaveBeenCalled()
+    })
+
+    test('Should return a list of surveys on success', async () => {
+        const { sut } = makeSut()
+        const surveys = await sut.load()
+        expect(surveys).toEqual(makeFakeSurveys())
+    })
+
+    test('Should throw if LoadSurveysRepository throws', async () => {
+        const { sut, loadSurveysRepositoryStub } = makeSut()
+        jest
+            .spyOn(loadSurveysRepositoryStub, 'loadAll')
+            .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        const promise = sut.load()
+        await expect(promise).rejects.toThrow()
     })
 })
